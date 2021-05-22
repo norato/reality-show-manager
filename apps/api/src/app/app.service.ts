f239b65d5f316bea815f4dd1a7eed4e3db171bbd
Felipe Norato Lacerda <fnoratol@gmail.com>
2021-05-22 12:26:38 -0300

    initial commit

---
+++ apps/api/src/app/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { Message } from '@reality-show-manager/api-interfaces';
+
+@Injectable()
+export class AppService {
+  getData(): Message {
+    return { message: 'Welcome to api!' };
+  }
+}
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/reality', '<rootDir>/apps/api'],
+};
